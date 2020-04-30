0a65e11cbe2c6814ccdcf758a85f279a8bd2a7af
AleBaccin <baccin.alessandro@gmail.com>
2020-04-30 01:17:58 +0100

    QoL for os agnostics paths

--- audio_tagging_utils.py
+++ audio_tagging_utils.py
@@ -7,44 +7,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib import figure
 from tqdm import tqdm
+import os 
 
 def append_ext(fn):
     return fn.replace('.wav', '.jpg')
 
 def create_spectrogram(filename,name, folder):
-    plt.interactive(False)
-    sample_rate = 16000
-    clip, _ = librosa.load(filename, sr=16000, res_type='kaiser_fast')
-    input_length = 2*sample_rate
-
-    if len(clip) > input_length:
-        max_offset = len(clip) - input_length
-        offset = np.random.randint(max_offset)
-        clip = clip[offset:(input_length+offset)]
-    else:
-        if input_length > len(clip):
-            max_offset = input_length - len(clip)
-            offset = np.random.randint(max_offset)
-        else:
-            offset = 0
-        clip = np.pad(clip, (offset, input_length - len(clip) - offset), "constant")
-
-    fig = plt.figure(figsize=[0.72,0.72])
-    ax = fig.add_subplot(111)
-    ax.axes.get_xaxis().set_visible(False)
-    ax.axes.get_yaxis().set_visible(False)
-    ax.set_frame_on(False)
-    S = librosa.feature.melspectrogram(y=clip, sr=sample_rate)
-    librosa.display.specshow(librosa.power_to_db(S, ref=np.max))
-    filename  = 'images\\' + folder + '\\' + name + '.jpg'
-    plt.savefig(filename, dpi=400, bbox_inches='tight',pad_inches=0)
-    plt.close()    
-    fig.clf()
-    plt.close(fig)
-    plt.close('all')
-    del filename,name,clip,sample_rate,fig,ax,S
-
-def create_spectrogram_no_prepro(filename,name, folder):
     plt.interactive(False)
     clip, sample_rate = librosa.load(filename, sr=None)
     fig = plt.figure(figsize=[0.72,0.72])
@@ -54,7 +22,7 @@ def create_spectrogram_no_prepro(filename,name, folder):
     ax.set_frame_on(False)
     S = librosa.feature.melspectrogram(y=clip, sr=sample_rate)
     librosa.display.specshow(librosa.power_to_db(S, ref=np.max))
-    filename  = 'images\\' + folder + '\\' + name + '.jpg'
+    filename  = os.path.join('images', folder, f'{name}.jpg')
     plt.savefig(filename, dpi=400, bbox_inches='tight',pad_inches=0)
     plt.close()    
     fig.clf()
@@ -62,27 +30,17 @@ def create_spectrogram_no_prepro(filename,name, folder):
     plt.close('all')
     del filename,name,clip,sample_rate,fig,ax,S
     
-# create_images('train', 'train_no_preprocessing')
-def create_images_no_prepro(input_dir, output_dir):
-    Data_dir=np.array(glob(f"{input_dir}\\*"))
+def create_images(input_dir, output_dir):
+    Data_dir=np.array(glob(os.path.join(input_dir, '*')))
     
     with tqdm(total= Data_dir.shape[0], position=0, leave=True) as pbar:
         for file in tqdm(Data_dir, position=0, leave=True):
-            filename,name = file, file.split('\\')[-1].split('.')[0]
-            create_spectrogram_no_prepro(filename,name, output_dir)
-            pbar.update()
-            
-def create_images_prepro(input_dir, output_dir):
-    Data_dir=np.array(glob(f"{input_dir}\\*"))
-    
-    with tqdm(total= Data_dir.shape[0], position=0, leave=True) as pbar:
-        for file in tqdm(Data_dir, position=0, leave=True):
-            filename,name = file, file.split('\\')[-1].split('.')[0]
+            filename,name = file, file.split(os.sep)[-1].split('.')[0]
             create_spectrogram(filename,name, output_dir)
             pbar.update()
             
 def create_mfcc_array(df, input_dir, sr, max_len, n_mfcc):
-    Data_dir=np.array(glob(f"{input_dir}\\*"))
+    Data_dir=np.array(glob(os.path.join(input_dir, '*')))
     
     mfcc_vectors = []
     labels = []
@@ -91,7 +49,7 @@ def create_mfcc_array(df, input_dir, sr, max_len, n_mfcc):
         pbar.set_description('Creating mfcc of %d .wav files' % len(df))
         for i, row in tqdm(df.iterrows(), position=0, leave=True):
             wavfile = row['fname']
-            mfcc = wav2mfcc(f'{input_dir}\\{wavfile}', n_mfcc= n_mfcc, sr= sr, max_len= max_len)
+            mfcc = wav2mfcc(os.path.join(input_dir, wavfile), n_mfcc= n_mfcc, sr= sr, max_len= max_len)
             mfcc_vectors.append(mfcc)
             labels.append(row['label'])
             pbar.update()
@@ -99,7 +57,7 @@ def create_mfcc_array(df, input_dir, sr, max_len, n_mfcc):
     return mfcc_vectors, labels
 
 def create_wav_array(df, input_dir, sr, max_len):
-    Data_dir=np.array(glob(f"{input_dir}\\*"))
+    Data_dir=np.array(glob(os.path.join(input_dir, '*')))
     
     wav_vectors = []
     labels = []
@@ -108,7 +66,7 @@ def create_wav_array(df, input_dir, sr, max_len):
         pbar.set_description('Creating np.array description of %d .wav files' % len(df))
         for i, row in tqdm(df.iterrows(), position=0, leave=True):
             wavfile = row['fname']
-            wave = wav(f'{input_dir}\\{wavfile}', sr= sr, max_len= max_len)
+            wave = wav(os.path.join(input_dir, wavfile), sr= sr, max_len= max_len)
             wav_vectors.append(wave)
             labels.append(row['label'])
             pbar.update()
@@ -140,12 +98,6 @@ def wav(file_path, sr, max_len):
         wave = np.pad(wave, (offset, true_length - len(wave) - offset), "constant")
         
     return wave
-
-def audio_norm(data):
-    max_data = np.max(data)
-    min_data = np.min(data)
-    data = (data-min_data)/(max_data-min_data+1e-6)
-    return data-0.5
 
 def mfcc_input_sizes(n_mfcc, sr, max_len):
     return (n_mfcc, 1 + int(np.floor((sr*max_len)/512)), 1)
